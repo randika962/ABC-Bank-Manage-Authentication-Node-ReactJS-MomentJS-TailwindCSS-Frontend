c042a8b6a44ccd928340ef608d64cb9e3c88c007
randika962 <srimalherath962@gmail.com>
2022-06-13 00:37:35 +0530

    Alets with Login & teransaction

--- src/components/Mytransaction.jsx
+++ src/components/Mytransaction.jsx
@@ -1,40 +1,29 @@
 // import React, { useEffect } from 'react'
 // import { Link } from "react-router-dom";
 // import { NavLink, useNavigate } from 'react-router-dom';
+// import jsPDF from 'jspdf';
+// import pdfMake from 'pdfmake';
+// import pdfFonts from 'pdfmake/build/vfs_fonts';
+// import htmlToPdfmake from 'html-to-pdfmake';
 import React from 'react'
 import profileImg from '../assets/profile.png'
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-// import { useRef } from 'react';
-// import { useReactToPrint } from 'react-to-print';
-import jsPDF from 'jspdf';
-import pdfMake from 'pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
-import htmlToPdfmake from 'html-to-pdfmake';
+import { useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
 
 
 export default function Mytransaction() {
 
-  // const componentRef = useRef();
-  // const handlePrint = useReactToPrint({
-  //       content: () => componentRef.current,
-  // });
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+  });
  
-  var user = JSON.parse(localStorage.getItem("user"))
-  var fName = localStorage.getItem("fName")
-  var jwt = localStorage.getItem("jwt")
-  const navigate = useNavigate();
-  const url = 'http://localhost:8080/bankusers/2'
+  var users = JSON.parse(localStorage.getItem("users"))
 
-  axios.post(url + fName, { headers: { "Authorization": `Bearer ${jwt}` } }).then(function (response) {
-
-    console.log(response.data);
-
-  })
-
-  const exportPdfServ = () => {
-    axios.get("http://localhost:8080/bankstransaction/ABC_Transaction/2")
+  const pdfexport = () => {
     console.log("Welcome pdf export service !")
+    axios.get("http://localhost:8080/bankstransaction/ABC_Transaction/2")
     console.log(axios.get("http://localhost:8080/bankstransaction/ABC_Transaction/2"))
   }
 
@@ -54,11 +43,14 @@ export default function Mytransaction() {
           <div className="text-base lg:flex-grow">
           </div>
           <div>
+            <button type="button" className="inline-block  px-4 py-2 leading-none border rounded font-semibold text-lg text-balck border-white hover:border-transparent hover:text-violet-400 hover:bg-white mt-4 lg:mt-0" onClick={ ()  => handlePrint() }>Print</button>
+          </div>
+          <div>
             <a href="/customerdash" className="inline-block  px-4 py-2 leading-none border rounded font-semibold text-lg text-balck border-white hover:border-transparent hover:text-violet-400 hover:bg-white mt-4 lg:mt-0">Back</a>
           </div>
         </div>
       </nav>
-      {/* <div ref={componentRef}> */}
+      <div refs={componentRef}>
       <div className="flex flex-wrap justify-center">
         <img
           src={profileImg}
@@ -66,10 +58,9 @@ export default function Mytransaction() {
           alt="..."
         />
         &nbsp;&nbsp;&nbsp;
-        <button type='button' className='w-40 h-10 my-5 py-2 text-center border-black bg-light bg-light bg-violet-300 hover:bg-slate-300 text-black font-semibold rounded-lg' onClick={ ()  => exportPdfServ() }>
+        {/* <button type="button" className='w-40 h-10 my-5 py-2 text-center border-black bg-light bg-light bg-violet-300 hover:bg-slate-300 text-black font-semibold rounded-lg' onClick={ ()  => pdfexport() }>
                   PDF Export
-        </button> 
-        {/* <img className='w-full min-h-full object-cover' src={addash1Img} alt="" /> */}
+        </button>  */}
         </div>
       <div>
         <br></br><br></br>
@@ -81,25 +72,55 @@ export default function Mytransaction() {
       </div>
       <div>
         <br></br><br></br>
-        <span className="font-semibold text-2xl tracking-tight">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Transaction Type :-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`${user.bankaccount[0].transaction[0].transacTypes}`}</span>
+        <span className="font-semibold text-2xl tracking-tight">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Transaction Type :-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`${user.bankaccount[0].transaction[0].transacType}`}</span>
       </div>
       <div>
         <br></br><br></br>
-        <span className="font-semibold text-2xl tracking-tight">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Transaction Time :-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`${user.bankaccount[0].transaction[0].transaconTime}`}</span>
-      </div>
-      <div>
-        <br></br><br></br>
-        <span className="font-semibold text-2xl tracking-tight">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Source Account ID :-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`${user.bankaccount[0].transaction[0].sourcesAccId}`}</span>
+        <span className="font-semibold text-2xl tracking-tight">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Source Account ID :-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`${user.bankaccount[0].transaction[0].sourceAccId}`}</span>
       </div>
       <div>
         <br></br><br></br>
         <span className="font-semibold text-2xl tracking-tight">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Destination Account ID :-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`${user.bankaccount[0].transaction[0].destinationAccId}`}</span>
       </div>
-      {/* </div> */}
+      </div>
     </div>
 
   );
 }
+
+
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------
+
+
+
+
+// var fName = localStorage.getItem("fName")
+// var jwt = localStorage.getItem("jwt")
+// const navigate = useNavigate();
+// const url = 'http://localhost:8080/bankusers/2'
+
+// axios.post(url + fName, { headers: { "Authorization": `Bearer ${jwt}` } }).then(function (response) {
+
+//   console.log(response.data);
+
+// })
+
+
+
+
+//-------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
 
   // useEffect(() => {
 
